00ad677b1cbc7413df38d9fc1937fdcc5155ff22
Jake Murphy <murph9y@gmail.com>
2020-09-25 17:17:09 +1000

    Add easier input method and an example

--- solver.py
+++ solver.py
@@ -12,10 +12,30 @@ class Solver:
             raise ValueError("No flasks given. format {'flasks':[...]}")
         flask_length = max([len(x) for x in parsed['flasks']])
         
-        return State([Flask([chr(x) for x in f], flask_length) for f in parsed['flasks']])
+        return State([Flask([x for x in f], flask_length) for f in parsed['flasks']])
 
-    def solve(self, parsed):
-        init_state = self.parse_object(parsed)
+    def parse_string(self, string):
+        flasks = []
+        rows = reversed(string.splitlines())
+        for r in rows:
+            if len(r) == 0:
+                continue
+            i = 0
+            for c in r:
+                if len(flasks)-1 < i:
+                    flasks.append([])
+                flasks[i].append(c)
+                i+=1
+
+        # then the final 2 rows
+        flasks.append([])
+        flasks.append([])
+
+        flask_length = max([len(x) for x in flasks])
+        return State([Flask([x for x in f], flask_length) for f in flasks])
+
+    def solve(self, state):
+        init_state = state
         init_state.validate()
     
         queue = [] #queue list
@@ -63,8 +83,8 @@ class Solver:
 ########################
 start_basic = {
     'flasks': [
-        [ord(x) for x in ['R', 'P', 'R']],
-        [ord(x) for x in ['P', 'R', 'P']],
+        [x for x in ['R', 'P', 'R']],
+        [x for x in ['P', 'R', 'P']],
         [],
         []
     ]
@@ -72,9 +92,9 @@ start_basic = {
 
 start_basic2 = {
     'flasks': [
-        [ord(x) for x in ['R', 'P', 'R']],
-        [ord(x) for x in ['P', 'R', 'P']],
-        [ord(x) for x in ['W', 'W', 'W']],
+        [x for x in ['R', 'P', 'R']],
+        [x for x in ['P', 'R', 'P']],
+        [x for x in ['W', 'W', 'W']],
         [],
         []
     ]
@@ -82,11 +102,11 @@ start_basic2 = {
 
 start_medium = {
     'flasks': [
-        [ord(x) for x in ['Y', 'R', 'B', 'R']],
-        [ord(x) for x in ['B', 'B', 'R', 'Y']],
-        [ord(x) for x in ['Y', 'R', 'B', 'Y']],
-        [ord(x) for x in ['4', '4', '3', '3']],
-        [ord(x) for x in ['3', '3', '4', '4']],
+        [x for x in ['Y', 'R', 'B', 'R']],
+        [x for x in ['B', 'B', 'R', 'Y']],
+        [x for x in ['Y', 'R', 'B', 'Y']],
+        [x for x in ['4', '4', '3', '3']],
+        [x for x in ['3', '3', '4', '4']],
         [],
         []
     ]
@@ -94,18 +114,18 @@ start_medium = {
 
 start_complex = {
     'flasks': [
-        [ord(x) for x in ['R', 'P', 'N', 'G']],
-        [ord(x) for x in ['T', 'L', 'O', 'L']],
-        [ord(x) for x in ['P', 'B', 'T', 'A']],
-        [ord(x) for x in ['B', 'G', 'N', 'V']],
-        [ord(x) for x in ['T', 'R', 'G', 'B']],
-        [ord(x) for x in ['O', 'A', 'Y', 'C']],
-        [ord(x) for x in ['Y', 'Y', 'R', 'V']],
-        [ord(x) for x in ['T', 'V', 'N', 'G']],
-        [ord(x) for x in ['N', 'L', 'L', 'V']],
-        [ord(x) for x in ['A', 'C', 'C', 'O']],
-        [ord(x) for x in ['A', 'R', 'P', 'B']],
-        [ord(x) for x in ['O', 'Y', 'C', 'P']],
+        [x for x in ['R', 'P', 'N', 'G']],
+        [x for x in ['T', 'L', 'O', 'L']],
+        [x for x in ['P', 'B', 'T', 'A']],
+        [x for x in ['B', 'G', 'N', 'V']],
+        [x for x in ['T', 'R', 'G', 'B']],
+        [x for x in ['O', 'A', 'Y', 'C']],
+        [x for x in ['Y', 'Y', 'R', 'V']],
+        [x for x in ['T', 'V', 'N', 'G']],
+        [x for x in ['N', 'L', 'L', 'V']],
+        [x for x in ['A', 'C', 'C', 'O']],
+        [x for x in ['A', 'R', 'P', 'B']],
+        [x for x in ['O', 'Y', 'C', 'P']],
         [],
         []
     ]
@@ -113,17 +133,17 @@ start_complex = {
 
 start_73 = {
     'flasks': [
-        [ord(x) for x in ['9', '0', '7', '1']],
-        [ord(x) for x in ['A', '9', '8', '1']],
-        [ord(x) for x in ['6', '0', '5', '2']],
-        [ord(x) for x in ['6', '5', 'A', '1']],
-        [ord(x) for x in ['0', '4', '3', '3']],
-        [ord(x) for x in ['8', '8', '5', '4']],
-        [ord(x) for x in ['3', '3', '6', '4']],
-        [ord(x) for x in ['0', '9', '2', '5']],
-        [ord(x) for x in ['1', '2', 'A', '6']],
-        [ord(x) for x in ['A', '8', '7', '4']],
-        [ord(x) for x in ['9', '7', '2', '7']],
+        [x for x in ['9', '0', '7', '1']],
+        [x for x in ['A', '9', '8', '1']],
+        [x for x in ['6', '0', '5', '2']],
+        [x for x in ['6', '5', 'A', '1']],
+        [x for x in ['0', '4', '3', '3']],
+        [x for x in ['8', '8', '5', '4']],
+        [x for x in ['3', '3', '6', '4']],
+        [x for x in ['0', '9', '2', '5']],
+        [x for x in ['1', '2', 'A', '6']],
+        [x for x in ['A', '8', '7', '4']],
+        [x for x in ['9', '7', '2', '7']],
         [],
         []
     ]
@@ -132,7 +152,14 @@ start_73 = {
 
 def run():
     solver = Solver()
-    result = solver.solve(start_medium)
+    state = solver.parse_string("""
+PPZAZRWSWRSA
+BYMSNPBRSPOW
+BGAMYWYBNMBY
+YNGPGOAOOMNZ
+GMRAORNZWZSG""")
+    # state = solver.parse_object(start_basic2)
+    result = solver.solve(state)
     solver.pretty_print_solution(result)
 
 
